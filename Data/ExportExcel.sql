8a27313d45b396ce71962b9f21ffae6538fe74a6
haint72 <ngothanhhai1972@gmail.com>
2022-11-14 22:42:47 +0700

    Bo sung PROC xuat du lieu ra file

--- Data/ExportExcel.sql
+++ Data/ExportExcel.sql
@@ -1,29 +1,17 @@
---
 -- show advanced options
-EXEC sp_configure 'show advanced options', 1
-GO
-RECONFIGURE
-GO
--- enable xp_cmdshell
-EXEC sp_configure 'xp_cmdshell', 1
-GO
-RECONFIGURE
-GO
--- hide advanced options
-EXEC sp_configure 'show advanced options', 0
-GO
-RECONFIGURE
-GO
--- 
-DECLARE @sql varchar(5000) = ''
-SET @sql = 'bcp "Select MSCV, SOCV, NGAYCV, NOIDUNG From [QLHS_HP].[dbo].tCongVan" queryout "D:\ExportData.xlsx" -w -T -S [192.168.1.222,1433] -U sa -P nth12345'
-
+EXEC master.dbo.sp_configure 'show advanced options', 1
+RECONFIGURE WITH OVERRIDE
+EXEC master.dbo.sp_configure 'xp_cmdshell', 1
+RECONFIGURE WITH OVERRIDE
+--SELECT @@SERVERNAME
+DECLARE @sql nvarchar(4000)
+SET @sql = 'bcp "SELECT MSCV, SOCV, NGAYCV, NOIDUNG FROM dbo.tCongVan" queryout "D:\ExportData.xls" -w -S "HAINT\SQLEXPRESS" -U sa -P nth12345 -d "QLHS_HP"'
 exec master..xp_cmdshell @sql
+GO
+-- hide xp_cmdshell
+EXEC master.dbo.sp_configure 'xp_cmdshell', 0
+RECONFIGURE WITH OVERRIDE
+EXEC master.dbo.sp_configure 'show advanced options', 0
+RECONFIGURE WITH OVERRIDE
 
 
---export xlsx use ACE.OLEDB.12.0
-INSERT INTO OPENROWSET('Microsoft.ACE.OLEDB.12.0', 'Excel 12.0;Database=D:\Solution\SQL\SQLExtra\ExportExcel\ExportExcelC2.xlsx;', 'SELECT * FROM [Sheet1$]')
-SELECT * FROM [DESKTOP-U30OAKD].[ManagerStudent].[dbo].Student
-
-SELECT @@SERVERNAME
-USE QLHS_DN
